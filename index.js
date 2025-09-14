e873725b4c5f14f4e2d11b0a90c9cdccad1eaf3a
navaneetjayaprakaash-stack <navaneetjayaprakaash@gmail.com>
2025-09-14 18:45:28 +0100

    Create index.js

---
+++ index.js
@@ -0,0 +1,57 @@
+const express = require('express');
+const http = require('http');
+const { Server } = require('socket.io');
+const path = require('path');
+
+const app = express();
+const server = http.createServer(app);
+const io = new Server(server);
+
+// Serve static files from "public" folder
+app.use(express.static(path.join(__dirname, 'public')));
+
+// Keep track of users
+let users = {}; // {socket.id: username}
+
+io.on('connection', (socket) => {
+    // Join a room
+    socket.on('joinRoom', ({ room, username }) => {
+        users[socket.id] = username;
+        socket.join(room);
+
+        // Notify room
+        socket.to(room).emit('message', { username: 'System', text: `${username} joined ${room}` });
+
+        // Send updated user list to room
+        const roomUsers = Array.from(io.sockets.adapter.rooms.get(room) || [])
+                               .map(id => users[id]);
+        io.to(room).emit('roomUsers', roomUsers);
+    });
+
+    // Leave a room
+    socket.on('leaveRoom', (room) => {
+        socket.leave(room);
+    });
+
+    // Public chat message
+    socket.on('chatMessage', ({ room, text, username }) => {
+        io.to(room).emit('message', { username, text });
+    });
+
+    // Private message (DM)
+    socket.on('privateMessage', ({ to, text, from }) => {
+        const targetSocketId = Object.keys(users).find(id => users[id] === to);
+        if(targetSocketId) {
+            io.to(targetSocketId).emit('message', { username: `(DM) ${from}`, text });
+        }
+    });
+
+    // Disconnect
+    socket.on('disconnect', () => {
+        delete users[socket.id];
+    });
+});
+
+// Start server
+const PORT = process.env.PORT || 3000;
+server.listen(PORT, () => console.log(`Server running on http://localhost:${PORT}`));
