62781fb1ab5faeee85e7bd13abfc4ad9a6b8b471
navaneetjayaprakaash-stack <navaneetjayaprakaash@gmail.com>
2025-11-02 10:08:53 +0000

    Create firebase-config.js

---
+++ public/firebase-config.js
@@ -0,0 +1,12 @@
+// public/firebase-config.js
+const firebaseConfig = {
+  apiKey: "AIzaSyDw-qqvRKmbu9R9b6sk70s4vbxJt-H0NGk",
+  authDomain: "my-chat-room-1d84f.firebaseapp.com",
+  projectId: "my-chat-room-1d84f",
+  storageBucket: "my-chat-room-1d84f.firebasestorage.app",
+  messagingSenderId: "747796328971",
+  appId: "1:747796328971:web:beb5c15265855e169e8d0e"
+};
+firebase.initializeApp(firebaseConfig);
+const auth = firebase.auth();
+const db = firebase.firestore();
