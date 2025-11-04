00eed12e11600df823c7aa54d8f2a507cc0f1b01
navaneetjayaprakaash-stack <navaneetjayaprakaash@gmail.com>
2025-11-04 18:59:00 +0000

    Update index.js

--- index.js
+++ index.js
@@ -9,22 +9,41 @@ const io = new Server(server);
 
 app.use(express.static(path.join(__dirname, "public")));
 
+// Track connected users
 const users = {};
 
+// Helper: get users in a room
+function getUsersInRoom(room) {
+  return Object.entries(users)
+    .filter(([id, user]) => user.room === room)
+    .map(([id, user]) => ({ id, username: user.username }));
+}
+
+// Socket.IO
 io.on("connection", (socket) => {
+  console.log("A user connected:", socket.id);
+
+  // Join room
   socket.on("joinRoom", ({ username, room }) => {
     users[socket.id] = { username, room };
     socket.join(room);
 
+    // Welcome message to self
     socket.emit("chatMessage", { user: "System", text: `Welcome to ${room}, ${username}! 🎉` });
+
+    // Notify others
     socket.to(room).emit("chatMessage", { user: "System", text: `${username} joined the room` });
+
+    // Update user list
     io.to(room).emit("userList", getUsersInRoom(room));
   });
 
+  // Chat message
   socket.on("chatMessage", (msg) => {
     io.to(msg.room).emit("chatMessage", msg);
   });
 
+  // Private message (DM)
   socket.on("privateMessage", ({ to, text }) => {
     const fromUser = users[socket.id];
     if (fromUser && users[to]) {
@@ -33,15 +52,19 @@ io.on("connection", (socket) => {
     }
   });
 
+  // Change username
   socket.on("changeUsername", (newUsername) => {
-    if (users[socket.id]) {
-      const { username: oldUsername, room } = users[socket.id];
-      users[socket.id].username = newUsername;
-      io.to(room).emit("chatMessage", { user: "System", text: `${oldUsername} changed username to ${newUsername}` });
+    const user = users[socket.id];
+    if (user) {
+      const oldUsername = user.username;
+      user.username = newUsername;
+      const room = user.room;
+      io.to(room).emit("chatMessage", { user: "System", text: `${oldUsername} changed name to ${newUsername}` });
       io.to(room).emit("userList", getUsersInRoom(room));
     }
   });
 
+  // Disconnect
   socket.on("disconnect", () => {
     if (users[socket.id]) {
       const { username, room } = users[socket.id];
@@ -52,12 +75,6 @@ io.on("connection", (socket) => {
   });
 });
 
-function getUsersInRoom(room) {
-  return Object.entries(users)
-    .filter(([id, u]) => u.room === room)
-    .map(([id, u]) => ({ id, username: u.username }));
-}
-
+// Server start
 const PORT = process.env.PORT || 5000;
 server.listen(PORT, () => console.log(`Server running on port ${PORT}`));
-
