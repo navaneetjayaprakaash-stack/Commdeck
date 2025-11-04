fa3b6f26fd728719b54726f5a7c4fb95752345d2
navaneetjayaprakaash-stack <navaneetjayaprakaash@gmail.com>
2025-11-04 18:30:15 +0000

    Update firebase.js

--- firebase.js
+++ firebase.js
@@ -1,7 +1,7 @@
+// Firebase setup
 import { initializeApp } from "firebase/app";
 import { getFirestore, collection, addDoc, query, orderBy, onSnapshot } from "firebase/firestore";
 
-// Firebase config
 const firebaseConfig = {
   apiKey: "AIzaSyDw-qqvRKmbu9R9b6sk70s4vbxJt-H0NGk",
   authDomain: "my-chat-room-1d84f.firebaseapp.com",
@@ -11,7 +11,6 @@ const firebaseConfig = {
   appId: "1:747796328971:web:beb5c15265855e169e8d0e"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
 export { collection, addDoc, query, orderBy, onSnapshot };
