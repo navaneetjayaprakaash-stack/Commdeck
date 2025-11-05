eec8baac110ef144725e7f8ad3c759136e8ddc64
navaneetjayaprakaash-stack <navaneetjayaprakaash@gmail.com>
2025-11-05 19:58:00 +0000

    Update firebase.js

--- public/firebase.js
+++ public/firebase.js
@@ -1,14 +1,16 @@
 import { initializeApp } from "firebase/app";
 import { getFirestore } from "firebase/firestore";
 
+// Firebase configuration
 const firebaseConfig = {
   apiKey: "AIzaSyDw-qqvRKmbu9R9b6sk70s4vbxJt-H0NGk",
   authDomain: "my-chat-room-1d84f.firebaseapp.com",
   projectId: "my-chat-room-1d84f",
-  storageBucket: "my-chat-room-1d84f.firebasestorage.app",
+  storageBucket: "my-chat-room-1d84f.appspot.com",
   messagingSenderId: "747796328971",
   appId: "1:747796328971:web:beb5c15265855e169e8d0e"
 };
 
+// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
