f92038d416753b427352f0d9fa2dcc3067b54948
navaneetjayaprakaash-stack <navaneetjayaprakaash@gmail.com>
2025-11-06 18:19:12 +0000

    Update firebase.js

--- public/firebase.js
+++ public/firebase.js
@@ -1,14 +1,24 @@
-import { initializeApp } from "firebase/app";
-import { getFirestore } from "firebase/firestore";
+// public/firebase.js
+import { initializeApp } from "https://www.gstatic.com/firebasejs/10.7.1/firebase-app.js";
+import { 
+  getFirestore, doc, setDoc, getDoc, updateDoc, arrayUnion, onSnapshot 
+} from "https://www.gstatic.com/firebasejs/10.7.1/firebase-firestore.js";
 
-export const firebaseConfig = {
-  apiKey: "YOUR_KEY",
-  authDomain: "YOUR_DOMAIN",
-  projectId: "YOUR_PROJECT_ID",
-  storageBucket: "YOUR_BUCKET",
-  messagingSenderId: "YOUR_SENDER",
-  appId: "YOUR_APP_ID"
+const firebaseConfig = {
+  apiKey: "AIzaSyDw-qqvRKmbu9R9b6sk70s4vbxJt-H0NGk",
+  authDomain: "my-chat-room-1d84f.firebaseapp.com",
+  projectId: "my-chat-room-1d84f",
+  storageBucket: "my-chat-room-1d84f.firebasestorage.app",
+  messagingSenderId: "747796328971",
+  appId: "1:747796328971:web:beb5c15265855e169e8d0e"
 };
 
-const app = initializeApp(firebaseConfig);
+// Init
+export const app = initializeApp(firebaseConfig);
 export const db = getFirestore(app);
+
+// Generate persistent user ID
+export function getUserID() {
+  let id = localStorage.getItem("chatUserID");
+  if (!id) {
+    id = M
